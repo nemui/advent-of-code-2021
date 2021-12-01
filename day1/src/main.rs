75eb8a3709077af71a801e100e96e8c8d788618a
Dmitry Mamchur <dmitry.mamchur@embark-studios.com>
2021-12-01 19:09:42 +0100

    range

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Ok(lines) = common::read_lines("input.txt") {
+    if let Ok(lines) = common::read_lines("day1/input.txt") {
         let numbers: Vec<i32> = lines
             .filter_map(|line| line.ok())
             .filter_map(|line| line.parse::<i32>().ok())
@@ -15,14 +15,9 @@ fn main() {
         );
         println!(
             "{}",
-            numbers
-                .iter()
-                .enumerate()
-                .take(numbers.len() - 2)
-                .filter(
-                    |(index, _)| numbers.iter().skip(*index).take(3).sum::<i32>()
-                        < numbers.iter().skip(index + 1).take(3).sum()
-                )
+            (0..numbers.len() - 2)
+                .filter(|index| numbers.iter().skip(*index).take(3).sum::<i32>()
+                    < numbers.iter().skip(index + 1).take(3).sum())
                 .count()
         );
     }
