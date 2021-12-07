823657044d1c742834f2959a50b8ed01e14bf7a5
Dmitry Mamchur <dmitry.mamchur@embark-studios.com>
2021-12-07 08:48:59 +0100

    day 7

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,13 +1,13 @@
 pub fn solve_1(filename: &str) -> u32 {
     if let Ok(mut lines) = common::read_lines(filename) {
-        let fish = common::list_of_numbers(lines.next().unwrap().unwrap(), ',');    
+        let fish = common::list_of_numbers(lines.next().unwrap().unwrap(), ',');
         return fish.len() as u32;
     }
     0
 }
 
 fn main() {
-    println!("{}", solve_1("day6/input.txt"));    
+    println!("{}", solve_1("day6/input.txt"));
 }
 
 #[cfg(test)]
@@ -17,5 +17,5 @@ mod tests {
     #[test]
     fn it_works_1() {
         assert_eq!(5934, solve_1("test_input.txt"));
-    }   
+    }
 }
